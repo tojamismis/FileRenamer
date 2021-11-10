34e54ac303c31dd2481e5f2c839c3c7af58c6427
Torin Blair <tojamismis@msn.com>
2021-11-09 22:08:28 -0700

    Update to c# 10 language support

--- FileRenamer/DateRenamer.cs
+++ FileRenamer/DateRenamer.cs
@@ -1,102 +1,99 @@
 ﻿using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
 
-namespace FileRenamer
+namespace FileRenamer;
+
+class DateRenamer
 {
-  class DateRenamer
-  {
     private readonly ILogger logger;
     public DateRenamer(ILogger<DateRenamer> logger)
     {
-      this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
     private const string newPathFolder = @"C:\Users\tojam\Videos\Audials\Audials Movies\AtiaICloud";
     public void Rename(string folderPath)
     {
-      foreach (string fileName in System.IO.Directory.GetFiles(@"C:\Users\tojam\iCloudPhotos\Photos"))
-      {
-        MoveFile(fileName, newPathFolder);
-      }
+        foreach (string fileName in System.IO.Directory.GetFiles(folderPath))
+        {
+            MoveFile(fileName, newPathFolder);
+        }
     }
 
     private void MoveFile(string fileName, string folderPath)
     {
-      try
-      {
-        logger.LogDebug($"Starting file {fileName}");
-        var fileDate = File.GetLastWriteTime(fileName);
-        var newName = CreateFileName(fileName, folderPath);
-        if (FilesEqual(fileName, newName))
+        try
         {
-          logger.LogDebug("New File Name of {0} already exists.  Will move to next file.", newName);
+            logger.LogDebug($"Starting file {fileName}");
+            var fileDate = File.GetLastWriteTime(fileName);
+            var newName = CreateFileName(fileName, folderPath);
+            if (FilesEqual(fileName, newName))
+            {
+                logger.LogDebug("New File Name of {0} already exists.  Will move to next file.", newName);
+            }
+            logger.LogDebug($"File {fileName} has new Destination {newName}");
+            if (!File.Exists(newName))
+            {
+                logger.LogDebug($"Copying {fileName} to {newName}");
+                File.Copy(fileName, newName);
+                logger.LogDebug($"Copy of {fileName} complete");
+            }
         }
-        logger.LogDebug($"File {fileName} has new Destination {newName}");
-        if (!File.Exists(newName))
+        catch (Exception ex)
         {
-          logger.LogDebug($"Copying {fileName} to {newName}");
-          File.Copy(fileName, newName);
-          logger.LogDebug($"Copy of {fileName} complete");
+            logger.LogError(ex, "Error with file {0}", fileName);
         }
-      }
-      catch(Exception ex)
-      {
-        logger.LogError(ex, "Error with file {0}", fileName);
-      }
     }
 
     private string CreateFileName(string existingFileName, string directoryPath)
     {
-      var ext = Path.GetExtension(existingFileName);
-      var newFileBaseName = CreateBaseFileDateName(existingFileName);
-      var newBasePath = Path.Combine(directoryPath, newFileBaseName);
-      var returnFileName = newBasePath + ext;
-      if (!File.Exists(returnFileName)) return returnFileName;
-      int i = 1;
-      while(File.Exists(returnFileName))
-      {
-        if (FilesEqual(existingFileName, returnFileName)) return returnFileName;
-        returnFileName = $"{newBasePath}-{i}{ext}";
-        i++;
-      }
-      return returnFileName;
+        var ext = Path.GetExtension(existingFileName);
+        var newFileBaseName = CreateBaseFileDateName(existingFileName);
+        var newBasePath = Path.Combine(directoryPath, newFileBaseName);
+        var returnFileName = newBasePath + ext;
+        if (!File.Exists(returnFileName)) return returnFileName;
+        int i = 1;
+        while (File.Exists(returnFileName))
+        {
+            if (FilesEqual(existingFileName, returnFileName)) return returnFileName;
+            returnFileName = $"{newBasePath}-{i}{ext}";
+            i++;
+        }
+        return returnFileName;
     }
 
     private string CreateBaseFileDateName(string fileName)
     {
-      var fileDate = File.GetLastWriteTime(fileName);
-      return fileDate.ToString("s").Replace("T", "_").Replace(":", ".");
+        var fileDate = File.GetLastWriteTime(fileName);
+        return fileDate.ToString("s").Replace("T", "_").Replace(":", ".");
     }
 
     private bool FilesEqual(string fileName, string newFileName)
     {
-      if (!File.Exists(newFileName)) return false;
-      var fileHash = CalcHash(fileName);
-      var newFileHash = CalcHash(newFileName);
-      var equal = fileHash.SequenceEqual(newFileHash);
-      Array.Clear(fileHash, 0, fileHash.Length);
-      Array.Clear(newFileHash, 0, newFileHash.Length);
-      return equal;
+        if (!File.Exists(newFileName)) return false;
+        var fileHash = CalcHash(fileName);
+        var newFileHash = CalcHash(newFileName);
+        var equal = fileHash.SequenceEqual(newFileHash);
+        Array.Clear(fileHash, 0, fileHash.Length);
+        Array.Clear(newFileHash, 0, newFileHash.Length);
+        return equal;
     }
 
     private byte[] CalcHash(string fileName)
     {
-      using var md5 = MD5.Create();
-      using var stream = File.OpenRead(fileName);
-      return md5.ComputeHash(stream);
+        using var md5 = MD5.Create();
+        using var stream = File.OpenRead(fileName);
+        return md5.ComputeHash(stream);
     }
 
     private void Rename(string folderPath, string findString, string replaceString)
     {
-      foreach (string fileName in System.IO.Directory.GetFiles(folderPath))
-      {
-        File.Move(fileName, fileName.Replace(findString, replaceString));
-      }
+        foreach (string fileName in System.IO.Directory.GetFiles(folderPath))
+        {
+            File.Move(fileName, fileName.Replace(findString, replaceString));
+        }
     }
-  }
 }
--- FileRenamer/Program.cs
+++ FileRenamer/Program.cs
@@ -4,53 +4,52 @@ using Serilog;
 using System;
 using System.IO;
 
-namespace FileRenamer
+namespace FileRenamer;
+
+class Program
 {
-  class Program
-  {
     static void Main(string[] args)
     {
-      var provider = BuildServices();
-      Console.WriteLine("Please Select:");
-      Console.WriteLine("1 - DateRenaming");
-      Console.WriteLine("2 - Tv Episode Renaming");
-      var input = Console.ReadLine();
-      if (input == "1")
-      {
-        var renamer = provider.GetRequiredService<DateRenamer>();
-        renamer.Rename(string.Empty);
-      }
-      else
-      {
-        Console.WriteLine("Please specify the root file path");
-        var path = Console.ReadLine();
-        if(!Directory.Exists(path))
+        var provider = BuildServices();
+        Console.WriteLine("Please Select:");
+        Console.WriteLine("1 - DateRenaming");
+        Console.WriteLine("2 - Tv Episode Renaming");
+        var input = Console.ReadLine();
+        if (input == "1")
         {
-          Console.WriteLine("Invalid Path Specified");
-          return;
+            var renamer = provider.GetRequiredService<DateRenamer>();
+            renamer.Rename(string.Empty);
         }
-        var renamer = provider.GetRequiredService<TvRenamer>();
-        renamer.Rename(path);
-      }
-      Console.WriteLine("Process Completed");
+        else
+        {
+            Console.WriteLine("Please specify the root file path");
+            var path = Console.ReadLine();
+            if (!Directory.Exists(path))
+            {
+                Console.WriteLine("Invalid Path Specified");
+                return;
+            }
+            var renamer = provider.GetRequiredService<TvRenamer>();
+            renamer.Rename(path);
+        }
+        Console.WriteLine("Process Completed");
     }
 
     static IServiceProvider BuildServices()
     {
-      var logFile = "Logs\\FileRenamer.log";
-      var services = new ServiceCollection();
-      services.AddLogging((builder) =>
-      {
-        builder.AddSerilog(new LoggerConfiguration()
-          .MinimumLevel.Verbose()
-          .WriteTo.File(logFile)
-          .WriteTo.Console()
-          .CreateLogger());
-        builder.SetMinimumLevel(LogLevel.Trace);
-      });
-      services.AddSingleton<DateRenamer>();
-      services.AddSingleton<TvRenamer>();
-      return services.BuildServiceProvider();
+        var logFile = "Logs\\FileRenamer.log";
+        var services = new ServiceCollection();
+        services.AddLogging((builder) =>
+        {
+            builder.AddSerilog(new LoggerConfiguration()
+        .MinimumLevel.Verbose()
+        .WriteTo.File(logFile)
+        .WriteTo.Console()
+        .CreateLogger());
+            builder.SetMinimumLevel(LogLevel.Trace);
+        });
+        services.AddSingleton<DateRenamer>();
+        services.AddSingleton<TvRenamer>();
+        return services.BuildServiceProvider();
     }
-  }
 }
